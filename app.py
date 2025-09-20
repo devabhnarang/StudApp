3392f23a7676a413f10f7d52a85dbbd812294944
devabhnarang <narangdevabh@gmail.com>
2025-09-20 18:06:12 +0530

    Initial commit of StudApp

--- app.py
+++ app.py
@@ -314,5 +314,5 @@ def edit_profile(user_id):
         cur.close()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use Render's port if provided
+    port = int(os.environ.get('PORT', 5000))  
     app.run(host='0.0.0.0', port=port, debug=True)
